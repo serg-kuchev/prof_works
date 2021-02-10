5fd0beda914fba903f20421c76399a9c4fe7f6d7
Сергей Кучев <kuchevwork@yandex.ru>
2021-02-11 00:00:36 +0300

    рабочий код

---
+++ iterator.py
@@ -0,0 +1,30 @@
+import json
+import re
+
+class Wikilink:
+
+    def __init__(self, file_path, start):
+        self.file = open(file_path, encoding='utf-8')
+        self.json = json.load(self.file)
+        self.start = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+
+        if self.start == len(self.json):
+            raise StopIteration
+
+        country = self.json[self.start]['name']['common']
+        country_link_name = country.replace(' ', '_')
+        link = 'https://en.wikipedia.org/wiki/' + country_link_name
+        return country, link
+
+if __name__ == '__main__':
+    with open('links_file.txt', 'w', encoding='utf-8') as lf:
+
+        for country, item in Wikilink('countries.json', -1):
+            lf.write(str(country) + ' ' + str(item) + '\n')
+            # print(country, item)
