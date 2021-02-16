efa363e87bbf5e087ed08020559df984bd0509ff
Сергей Кучев <kuchevwork@yandex.ru>
2021-02-16 17:10:11 +0300

    декоратор с записью в файл

---
+++ decorator.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+def logger_decor(function):
+    def func_logger(*args, **kwargs):
+        date = datetime.now()
+        name = function.__name__
+        f_return = function(*args, **kwargs)
+
+        with open('logs.txt', 'w') as f:
+            f.write(f'{date} {name} {args} {kwargs} {f_return}')
+
+    return func_logger
+
+
+@logger_decor
+def some_func(*args, **kwargs):
+    return 'done'
+
+
+some_func('1')
